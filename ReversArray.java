e23d0b9ccf68c7999a830db4aab3207f0537eb18
tharaka <tharakanuwan1997@gmail.com>
2024-10-27 14:05:39 +0530

    revers array

---
+++ ReversArray.java
@@ -0,0 +1,22 @@
+public class ReversArray{
+	
+	public static void main(String arge[]){
+		
+		int[]arr={1,2,3,4,5};
+		
+		int n=arr.length;
+		for(int i=0;i<n/2;i++){
+			int temp;
+			temp=arr[i];
+			arr[i]=arr[n-i-1];
+			arr[n-i-1]=temp;
+			
+		}
+		
+		
+		for(int j=0;j<arr.length;j++){
+			System.out.println(arr[j]);
+		}
+	}
+	
+}
